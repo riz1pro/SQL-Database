c1b4d9f72e8641363299efe69a77a35e0c76d2b7
riz1pro <rizlapro1@live.co.uk>
2023-11-28 12:57:29 +0000

    Update Libary.sql

--- Libary.sql
+++ Libary.sql
@@ -17,34 +17,6 @@ CREATE TABLE LibrarySchema.LIBRARY_BRANCH (
     
 );
 
-CREATE TABLE LibrarySchema.BORROWER (
-    CardNo INT PRIMARY KEY IDENTITY(1,1),
-    Name NVARCHAR(100),
-    Address NVARCHAR(255),
-    Phone NVARCHAR(20)
-);
-
-CREATE TABLE LibrarySchema.PUBLISHER (
-    PublisherID INT PRIMARY KEY IDENTITY(1,1),
-    PublisherName NVARCHAR(100),
-    Address NVARCHAR(255),
-    Phone NVARCHAR(20)
-);
-
-CREATE TABLE LibrarySchema.BOOKS (
-    BookID INT PRIMARY KEY IDENTITY(1,1),
-    Title NVARCHAR(255),
-    PublisherName NVARCHAR(100),
-   
-);
-
-CREATE TABLE LibrarySchema.BOOK_AUTHORS (
-    AuthorID INT PRIMARY KEY IDENTITY(1,1),
-    AuthorName VARCHAR(100),
-    BookID INT, -- Foreign key column referencing BOOKS table
-    FOREIGN KEY (BookID) REFERENCES LibrarySchema.BOOKS(BookID)
-);
-
 CREATE TABLE LibrarySchema.BOOK_COPIES (
     CopyID INT PRIMARY KEY IDENTITY(1,1),
     Number_of_Copies INT,
@@ -54,6 +26,29 @@ CREATE TABLE LibrarySchema.BOOK_COPIES (
     FOREIGN KEY (BranchID) REFERENCES LibrarySchema.LIBRARY_BRANCH(BranchID)
 );
 
+
+CREATE TABLE LibrarySchema.BOOKS (
+    BookID INT PRIMARY KEY IDENTITY(1,1),
+    Title NVARCHAR(255),
+    PublisherName NVARCHAR(100),
+   
+);
+
+
+CREATE TABLE LibrarySchema.BOOK_AUTHORS (
+    AuthorID INT PRIMARY KEY IDENTITY(1,1),
+    AuthorName VARCHAR(100),
+    BookID INT, -- Foreign key column referencing BOOKS table
+    FOREIGN KEY (BookID) REFERENCES LibrarySchema.BOOKS(BookID)
+);
+
+
+CREATE TABLE LibrarySchema.BORROWER (
+    CardNo INT PRIMARY KEY IDENTITY(1,1),
+    Name NVARCHAR(100),
+    Address NVARCHAR(255),
+    Phone NVARCHAR(20)
+);
 
 CREATE TABLE LibrarySchema.BOOK_LOANS (
     LoanID INT PRIMARY KEY IDENTITY(1,1),
@@ -67,9 +62,24 @@ CREATE TABLE LibrarySchema.BOOK_LOANS (
     FOREIGN KEY (CardNo) REFERENCES LibrarySchema.BORROWER(CardNo)
 );
 
+CREATE TABLE LibrarySchema.PUBLISHER (
+    PublisherID INT PRIMARY KEY IDENTITY(1,1),
+    PublisherName NVARCHAR(100),
+    Address NVARCHAR(255),
+    Phone NVARCHAR(20)
+);
 
--- Populate Tables
-INSERT INTO LibrarySchema.LIBRARY_BRANCH ( BranchName, BranchAddress)
+
+
+
+
+
+
+
+
+SET IDENTITY_INSERT LibrarySchema.LIBRARY_BRANCH ON;
+
+INSERT INTO LibrarySchema.LIBRARY_BRANCH (  BranchName, BranchAddress)
 VALUES
     ('SInfin Branch', 'Dublin'),
     ('Derby Branch', 'England'),
@@ -79,33 +89,23 @@ VALUES
     ('Sharpstown Branch', 'London');
 
 
+   
 
 
-
-
-INSERT INTO LibrarySchema.BORROWER (Name, Address, Phone)
+    INSERT INTO LibrarySchema.BOOK_COPIES (BookID, BranchID, Number_of_Copies)
 VALUES
-    ('Tim', '14 Colts Foot', '123-456-7890'),
-    ('Asif', '17 Cloverdale', '123-456-7891'),
-    ('Kaz', '220 Elm Street', '123-456-7892'),
-    ('Kiran', '45 Rger Street', '123-456-7893'),
-    ('Mel', '52 Fleet Street', '123-456-7894'),
-    ('Steff', '23 Sydney Road', '123-456-7895'),
-    ('Paul', '1 Leeds Drive', '123-456-7896'),
-    ('Steve', '99 Jannah Way', '123-456-7897');
+    (1, 1, 95),  
+    (2, 2, 25),  
+    (3, 3, 55),  
+    (4, 4, 41),  
+    (5, 5, 54),  
+    (6, 6, 62),  
+    (7, 7, 13),  
+    (8, 8, 24),  
+    (9, 9, 32),  
+    (10, 10, 42);  
 
-INSERT INTO LibrarySchema.PUBLISHER (PublisherName, Address, Phone)
-VALUES
-    ('Penguin Random House', 'Penguin Building', '223-456-7890'),
-    ('Hachette Livre', 'Haven Building', '223-456-7891'),
-    ('HarperCollins', 'Collings Street', '323-456-7892'),
-    ('Pan Macmillan', 'Pan Road', '523-456-7893'),
-    ('Pearson Education', 'Education Building', '523-456-7894'),
-    ('Oxford University Press', 'Press Road', '023-456-7895'),
-    ('Bloomsbury, A&C Black', 'Black Building', '523-456-7896'),
-    ('Simon & Schuster', 'Sim City', '523-456-7897'),
-    ('John Wiley & Sons', 'Sons Building', '123-456-7898'),
-    ('Faber Independent Alliance', 'Faber Street', '823-456-7899');
+
 
 
 INSERT INTO LibrarySchema.BOOKS (Title, PublisherName)
@@ -131,9 +131,8 @@ VALUES
     ('The Picture of Dorian Gray', 'Bloomsbury, A&C Black'),
     ('The War of the Worlds', 'HarperCollins');
 
-    
 
-INSERT INTO LibrarySchema.BOOK_AUTHORS (AuthorName)
+    INSERT INTO LibrarySchema.BOOK_AUTHORS (AuthorName)
 VALUES
     ('Nora Ephron'),
     ('Alain Mabanckou'),
@@ -147,37 +146,105 @@ VALUES
     ('M John Harrison');
 
 
-
-
-
-INSERT INTO LibrarySchema.BOOK_COPIES ( BookID, BranchID, Number_of_Copies)
+INSERT INTO LibrarySchema.BORROWER (Name, Address, Phone)
 VALUES
-    ('5', '256', '9534'),
-    ('10', '636', '2548'),
-    ('12', '585', '5553'),
-    ('5', '555', '4147'),
-    ('22', '336', '5447'),
-    ('17', '447', '6222'),
-    ('29', '557', '1333'),
-    ('99', '785', '2448'),
-    ('45', '814', '3217'),
-    ('96', '145', '4258');
+    ('Tim', '14 Colts Foot', '123-456-7890'),
+    ('Asif', '17 Cloverdale', '123-456-7891'),
+    ('Kaz', '220 Elm Street', '123-456-7892'),
+    ('Kiran', '45 Rger Street', '123-456-7893'),
+    ('Mel', '52 Fleet Street', '123-456-7894'),
+    ('Steff', '23 Sydney Road', '123-456-7895'),
+    ('Paul', '1 Leeds Drive', '123-456-7896'),
+    ('Steve', '99 Jannah Way', '123-456-7897');
+
+
 
 
 
 
 INSERT INTO LibrarySchema.BOOK_LOANS (BookID, BranchID, CardNo, DateOut, DateDue)
 VALUES
-    ('5', '256',  '525', '2023-01-01', '2023-01-15'),
-    ('10', '636', '258', '2023-02-01', '2023-02-15'),
-    ('12', '585', '114', '2023-03-01', '2023-03-15'),
-    ('5', '555', '258', '2023-04-01', '2023-04-15'),
-    ('22', '336', '441', '2023-05-01', '2023-05-15'),
-    ('17', '447', '525', '2023-06-01', '2023-06-15'),
-    ('29', '557', '447', '2023-10-01', '2023-10-15'),
-    ('99', '785', '258', '2023-12-03', '2023-02-17'),
-    ('45', '814', '268',  '2023-18-08',  '2023-05-18'),
-    ('96', '145', '156',  '2023-15-07', '2023-08-19');
+    (1, 1, 1, '2023-01-01', '2023-01-15'),  
+    (2, 2, 2, '2023-02-01', '2023-02-15'),  
+    (3, 3, 3, '2023-03-01', '2023-03-15'), 
+    (4, 4, 4, '2023-04-01', '2023-04-15'),  
+    (5, 5, 5, '2023-05-01', '2023-05-15'),  
+    (6, 6, 6, '2023-06-01', '2023-06-15'),  
+    (7, 7, 7, '2023-10-01', '2023-10-15'),  
+    (8, 8, 8, '2023-12-03', '2023-02-17'),  
+    (9, 9, 9, '2023-08-18', '2023-05-18'),  
+    (10, 10, 10, '2023-07-15', '2023-08-19'); 
+
+
+
+
+
+INSERT INTO LibrarySchema.PUBLISHER (PublisherName, Address, Phone)
+VALUES
+    ('Penguin Random House', 'Penguin Building', '223-456-7890'),
+    ('Hachette Livre', 'Haven Building', '223-456-7891'),
+    ('HarperCollins', 'Collings Street', '323-456-7892'),
+    ('Pan Macmillan', 'Pan Road', '523-456-7893'),
+    ('Pearson Education', 'Education Building', '523-456-7894'),
+    ('Oxford University Press', 'Press Road', '023-456-7895'),
+    ('Bloomsbury, A&C Black', 'Black Building', '523-456-7896'),
+    ('Simon & Schuster', 'Sim City', '523-456-7897'),
+    ('John Wiley & Sons', 'Sons Building', '123-456-7898'),
+    ('Faber Independent Alliance', 'Faber Street', '823-456-7899');
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+-- Assuming BookID references BOOKS table
+ALTER TABLE LibrarySchema.BOOK_COPIES
+ADD CONSTRAINT fk_book_copies_bookID
+FOREIGN KEY (BookID)
+REFERENCES LibrarySchema.BOOKS(BookID)
+ON DELETE CASCADE;  -- or ON DELETE SET NULL, depending on your requirements
+
+
+
+
+
+-- Assuming BranchID references LIBRARY_BRANCH table
+ALTER TABLE LibrarySchema.BOOK_COPIES
+ADD CONSTRAINT fk_book_copies_branchID
+FOREIGN KEY (BranchID)
+REFERENCES LibrarySchema.LIBRARY_BRANCH(BranchID)
+ON DELETE CASCADE;  -- or ON DELETE SET NULL, depending on your requirements
+
+
+
+
+-- Adding FOREIGN KEY constraints for BookID and BranchID
+ALTER TABLE LibrarySchema.BOOK_LOANS
+ADD CONSTRAINT FK_BookID
+FOREIGN KEY (BookID)
+REFERENCES LibrarySchema.BOOKS(BookID);
+
+ALTER TABLE LibrarySchema.BOOK_LOANS
+ADD CONSTRAINT FK_BranchID
+FOREIGN KEY (BranchID)
+REFERENCES LibrarySchema.LIBRARY_BRANCH(BranchID);
+
+
+
+
+
+
+
+
+
 
 
 
@@ -186,7 +253,5 @@ VALUES
 
 SELECT BranchID, BranchName, BranchAddress
 FROM LibrarySchema. LIBRARY_BRANCH; 
-
-
 
 
